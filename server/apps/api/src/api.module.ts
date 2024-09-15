33792b92fe7a6a5000c6ab474401a700f35307ba
Mor <mortogo321@gmail.com>
2024-09-15 19:24:13 +0700

    init prisma

--- server/apps/api/src/api.module.ts
+++ server/apps/api/src/api.module.ts
@@ -1,3 +1,4 @@
+import { DatabaseModule } from '@app/common';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ApiController } from './api.controller';
@@ -9,6 +10,7 @@ import { ApiService } from './api.service';
       isGlobal: true,
       envFilePath: `${process.cwd()}/apps/api/.env.app`,
     }),
+    DatabaseModule,
   ],
   controllers: [ApiController],
   providers: [ApiService],
